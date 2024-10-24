3bcd4942fa458c3681df83eb538f68e3a409c498
Valerii Viun <2455521@gmail.com>
2024-10-24 16:56:55 +0300

    Update ListCatalog.styled.js

--- src/component/ListCatalog/ListCatalog.styled.js
+++ src/component/ListCatalog/ListCatalog.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const List = styled.ul`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
   flex-wrap: wrap;
   padding: 0px;
